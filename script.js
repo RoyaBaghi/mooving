9024dd689d3506869c536d71b25dd154e3941752
RoyaBaghi <royabaghi.bg@gmail.com>
2024-05-08 05:34:34 -0700

    finall changes

--- script.js
+++ script.js
@@ -5,5 +5,15 @@ window.addEventListener('scroll', checkBoxes)
 checkBoxes()
 
 function checkBoxes(){
-    
+    const triggerButtom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top
+
+        if(boxTop < triggerButtom) {
+            box.classList.add('show')
+        } else {
+            box.classList.remove('show')
+        }
+    })
 }
